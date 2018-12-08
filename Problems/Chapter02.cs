80ffa68c6563360b1ea98af105a27b7bc3888623
Cluckerino <elitedragonslayer@gmail.com>
2018-12-08 01:01:52 -0800

    Added 2.1 Stub

---
+++ Problems/Chapter02.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Problems
+{
+    public static class Chapter02
+    {
+        /// <summary>
+        /// Remove duplicates from the linked list.
+        /// </summary>
+        public static LinkedList<int> P01_RemoveDupes(LinkedList<int> input)
+        {
+            return new LinkedList<int>(new[] { 0 });
+        }
+    }
+}
---
+++ Tests/Chapter02Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Problems;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Chapter02Tests
+    {
+        public void P01_RemoveDupes(IEnumerable<int> input, IEnumerable<int> expected)
+        {
+            var inputList = new LinkedList<int>(input);
+            var expectedList = new LinkedList<int>(expected);
+            Assert.That(Chapter02.P01_RemoveDupes(inputList), Is.EqualTo(expectedList));
+        }
+
+        [Test]
+        public void P01_RemoveDupes_HasDupes() =>
+            P01_RemoveDupes(new[] { 3, 4, 5, 6, 3, 3, 5, 6, 7 }, new[] { 3, 4, 5, 6, 7 });
+
+        [Test]
+        public void P01_RemoveDupes_NoDupes() =>
+            P01_RemoveDupes(new[] { 3, 4, 5, 6, 7 }, new[] { 3, 4, 5, 6, 7 });
+    }
+}
