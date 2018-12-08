f01734463febe0d21c3e71f8497abcd0a83895f6
Cluckerino <elitedragonslayer@gmail.com>
2018-12-07 23:07:57 -0800

    Added example problem

---
+++ Problems/Chapter01.cs
@@ -0,0 +1,13 @@
+﻿namespace Problems
+{
+    public static class Chapter01
+    {
+        /// <summary>
+        /// Test if a string has all unique characters.
+        /// </summary>
+        public static bool P01_IsUnique(string input)
+        {
+            return false;
+        }
+    }
+}
---
+++ Tests/Chapter01Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Problems;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Chapter01Tests
+    {
+        [TestCase("qwertyuiopasdfghjklzxcvbnm", true)]
+        [TestCase("qwertyuiopasdfghjklzxcvbnmq", false)]
+        public void P01_IsUnique(string input, bool expected)
+        {
+            Assert.AreEqual(Chapter01.P01_IsUnique(input), expected);
+        }
+    }
+}
