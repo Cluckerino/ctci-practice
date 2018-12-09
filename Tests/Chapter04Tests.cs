da8a061da0938cd198fd76004bed765034518423
Cluckerino <elitedragonslayer@gmail.com>
2018-12-09 15:55:04 -0800

    Refactored example tree to fixture

--- Tests/Chapter04Tests.cs
+++ Tests/Chapter04Tests.cs
@@ -6,26 +6,35 @@ using System;
 [TestFixture]
 public class Chapter04Tests
 {
+    IntNode exTreeHead;
+
+    /// <summary>
+    /// Create example tree to test stuff.
+    /// </summary>
+    [SetUp]
+    public void CreateExampleTree()
+    {
+        exTreeHead = new IntNode(8);
+        exTreeHead.LVal = 4;
+        exTreeHead.L.LVal = 2;
+        exTreeHead.L.RVal = 6;
+        exTreeHead.RVal = 10;
+        exTreeHead.R.RVal = 20;
+    }
+
     [Test]
     public void BinaryTreeTest()
     {
-        var h = new IntNode(8);
-        h.LVal = 4;
-        h.L.LVal = 2;
-        h.L.RVal = 6;
-        h.RVal = 10;
-        h.R.RVal = 20;
-
-        var leftmost = h;
+        var leftmost = exTreeHead;
         while (!(leftmost.L is null))
             leftmost = leftmost.L;
 
-        var rightmost = h;
+        var rightmost = exTreeHead;
         while (!(rightmost.R is null))
             rightmost = rightmost.R;
 
         Console.WriteLine("Tree structure:");
-        Console.WriteLine(h);
+        Console.WriteLine(exTreeHead);
 
         Assert.That(leftmost.Value, Is.EqualTo(2));
         Assert.That(rightmost.Value, Is.EqualTo(20));
