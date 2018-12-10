a9a5cb6e0f1e77ffca594e3eba5a887b0d343395
Cluckerino <elitedragonslayer@gmail.com>
2018-12-10 12:34:43 -0800

    Stub for 8.1

---
+++ Problems/Chapter08.cs
@@ -0,0 +1,14 @@
+﻿namespace Problems
+{
+    public static class Chapter08
+    {
+        /// <summary>
+        /// Given a staircase with N steps, count how many ways you can up it if you can go 1, 2,
+        /// or 3 steps at a time.
+        /// </summary>
+        public static int P01TripleStep(int numSteps)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Tests/Chapter08Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Problems;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Chapter08Tests
+    {
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 4)]
+        [TestCase(4, 7)]
+        [TestCase(5, 13)]
+        [TestCase(6, 24)]
+        public void T01TripleStep(int input, int expected)
+        {
+            //! Testing cases:
+            //! Steps   Possibilities
+            //! 1       1
+            //! 2       11, 2
+            //! 3       111, 12, 21, 3
+            //! 4       1111, 112, 121, 13, 211, 22, 31
+            //! 5       11111, 1112, 1121. 113, 1211, 122, 131, 2111, 212, 221, 23, 311, 32
+
+            Assert.That(Chapter08.P01TripleStep(input), Is.EqualTo(expected));
+        }
+    }
+}
