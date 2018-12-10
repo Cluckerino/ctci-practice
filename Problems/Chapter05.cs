78a55345e849969e83ec22ed43c5beb4d7de4ec2
Cluckerino <elitedragonslayer@gmail.com>
2018-12-09 19:21:27 -0800

    Stub for 5.1

---
+++ Problems/Chapter05.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problems
+{
+    public static class Chapter05
+    {
+        /// <summary>
+        /// Create a binary string of this number.
+        /// </summary>
+        public static string AsBinary(this int num) => Convert.ToString(num, 2);
+
+        /// <summary>
+        /// Insert m into n at bits i-j inclusive.
+        /// </summary>
+        public static int P01Insertion(int n, int m, int i, int j)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Tests/Chapter05Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Chapter05Tests
+    {
+        [TestCase(0b10000000000, 0b10011, 2, 6, 0b10001001100)]
+        [TestCase(0b10001110100, 0b10011, 2, 6, 0b10001001100)]
+        [TestCase(0b10000000000, 0b10011, 2, 10, 0b00001001100)]
+        [TestCase(0b11111111111, 0b10011, 2, 6, 0b11111001111)]
+        public void T01Insertion(int n, int m, int i, int j, int expected)
+        {
+            int actual = Chapter05.P01Insertion(n, m, i, j);
+
+            Console.WriteLine(actual.AsBinary());
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
