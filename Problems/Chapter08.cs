9a0d83eefa8b08930ea8c50c25297a36f8f3f9cb
Cluckerino <elitedragonslayer@gmail.com>
2018-12-10 22:01:40 -0800

    8.1: code cleanup and parameterized test to see how slow the recursive solution is

--- Problems/Chapter08.cs
+++ Problems/Chapter08.cs
@@ -3,16 +3,14 @@
     public static class Chapter08
     {
         /// <summary>
-        /// Given a staircase with N steps, count how many ways you can up it if you can go 1, 2,
-        /// or 3 steps at a time.
+        /// Given a staircase with N steps, count how many ways you can up it if you can go 1, 2, or
+        /// 3 steps at a time. Iterative solution here.
         /// </summary>
-        public static int P01TripleStep(int numSteps)
+        public static int P01TripleStepIter(int numSteps)
         {
-            // Iterative solution here.
-
-            // Sum of ways = ways after taking 1 step + ways after taking 2 steps + ways after
-            // taking 3 steps. Essentially, S(N) = S(N-1) + S(N-2) + S(N-3), and the problem is a
-            // tribonnaci sequence.
+            // Sum of ways = ways after taking 1 step + ways after taking 2 steps + ways after taking
+            // 3 steps. Essentially, S(N) = S(N-1) + S(N-2) + S(N-3), and the problem is a tribonnaci
+            // sequence.
 
             // To initalize the sequence, define: S(0) = 1; S(N) = 0 if N < 0
             var n1 = 1;
@@ -32,20 +30,23 @@
         }
 
         /// <summary>
-        /// Recursive triple step answer.
+        /// Given a staircase with N steps, count how many ways you can up it if you can go 1, 2, or
+        /// 3 steps at a time. Slow, bulky recursive solution here.
         /// </summary>
-        public static int P01Recursive(int numStepsLeft)
+        public static int P01TripleStepRec(int numStepsLeft)
         {
             // Base cases, take the last 1, 2, or 3 steps, or recurse.
             if (numStepsLeft == 1)
                 return 1;
             else if (numStepsLeft == 2)
-                return 1 + P01Recursive(numStepsLeft - 1);
+                return 1 + P01TripleStepRec(numStepsLeft - 1);
             else if (numStepsLeft == 3)
-                return 1 + P01Recursive(numStepsLeft - 1) + P01Recursive(numStepsLeft - 2);
+                return 1 + P01TripleStepRec(numStepsLeft - 1) + P01TripleStepRec(numStepsLeft - 2);
 
-            // Everything else, imagine youre taking 1, 2, or 3 steps first. cout possibilities.
-            return P01Recursive(numStepsLeft - 1) + P01Recursive(numStepsLeft - 2) + P01Recursive(numStepsLeft - 3);
+            // Everything else, imagine youre taking 1, 2, or 3 steps first. Count possibilities.
+            return P01TripleStepRec(numStepsLeft - 1) +
+                P01TripleStepRec(numStepsLeft - 2) +
+                P01TripleStepRec(numStepsLeft - 3);
         }
     }
 }
--- Tests/Chapter08Tests.cs
+++ Tests/Chapter08Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using Problems;
 
 namespace Tests
@@ -6,13 +7,27 @@ namespace Tests
     [TestFixture]
     public class Chapter08Tests
     {
-        [TestCase(1, 1)]
-        [TestCase(2, 2)]
-        [TestCase(3, 4)]
-        [TestCase(4, 7)]
-        [TestCase(5, 13)]
-        [TestCase(6, 24)]
-        public void T01TripleStep(int input, int expected)
+        public static IEnumerable<TestCaseData> T01TripleStepCases
+        {
+            get
+            {
+                var useIterativeOptions = new [] { true, false };
+
+                foreach (var useIterative in useIterativeOptions)
+                {
+                    yield return new TestCaseData(1, useIterative, 1);
+                    yield return new TestCaseData(2, useIterative, 2);
+                    yield return new TestCaseData(3, useIterative, 4);
+                    yield return new TestCaseData(4, useIterative, 7);
+                    yield return new TestCaseData(5, useIterative, 13);
+                    yield return new TestCaseData(26, useIterative, 4700770);
+                    yield return new TestCaseData(27, useIterative, 8646064);
+                }
+            }
+        }
+
+        [TestCaseSource(typeof(Chapter08Tests), nameof(T01TripleStepCases))]
+        public void T01TripleStep(int input, bool useIterative, int expected)
         {
             //! Testing cases:
             //! Steps   Possibilities
@@ -22,11 +37,11 @@ namespace Tests
             //! 4       1111, 112, 121, 13, 211, 22, 31
             //! 5       11111, 1112, 1121. 113, 1211, 122, 131, 2111, 212, 221, 23, 311, 32
 
-            Assert.Multiple(() =>
-            {
-                Assert.That(Chapter08.P01TripleStep(input), Is.EqualTo(expected));
-                Assert.That(Chapter08.P01Recursive(input), Is.EqualTo(expected));
-            });
+            int actual = useIterative ?
+                Chapter08.P01TripleStepIter(input) :
+                Chapter08.P01TripleStepRec(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
