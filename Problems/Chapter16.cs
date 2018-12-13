4301afa9ad256caca68dfc5d1f28c4fd69d3f7e4
Cluckerino <elitedragonslayer@gmail.com>
2018-12-12 22:29:31 -0800

    Stub for 16.1

---
+++ Problems/Chapter16.cs
@@ -0,0 +1,11 @@
+﻿namespace Problems
+{
+    public static class Chapter16
+    {
+        /// <summary>
+        /// Swap 'em without temp variables.
+        /// </summary>
+        public static void P01InPlaceSwap(ref int a, ref int b)
+        { }
+    }
+}
---
+++ Tests/Chapter16Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Chapter16Tests
+    {
+        [Test]
+        public void T01InPlaceSwap()
+        {
+            var rng = new Random();
+
+            var a = rng.Next(int.MinValue / 2, int.MaxValue / 2);
+            var b = rng.Next(int.MinValue / 2, int.MaxValue / 2);
+
+            var expectedA = b;
+            var expectedB = a;
+
+            Chapter16.P01InPlaceSwap(ref a, ref b);
+
+            Assert.That(a, Is.EqualTo(expectedA));
+            Assert.That(b, Is.EqualTo(expectedB));
+        }
+    }
+}
