85b88d1e20f7efd2e0b222e0a26beea28b27799c
Cluckerino <elitedragonslayer@gmail.com>
2018-12-12 22:35:31 -0800

    Solution for 16.1

--- Problems/Chapter16.cs
+++ Problems/Chapter16.cs
@@ -6,6 +6,11 @@
         /// Swap 'em without temp variables.
         /// </summary>
         public static void P01InPlaceSwap(ref int a, ref int b)
-        { }
+        {
+            if (a == b) return;
+            a = b - a;
+            b = b - a;
+            a = a + b;
+        }
     }
 }
