facdea0d3fbe869e7714f56282958194e9ff77a7
Cluckerino <elitedragonslayer@gmail.com>
2018-12-13 13:52:40 -0800

    Migrated Graph code to Common

---
+++ Common/SetGraph.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace Common
+{
+    /// <summary>
+    /// Graph where values are unique.
+    /// </summary>
+    /// <typeparam name="T">The node's value type.</typeparam>
+    public class SetGraph<T>
+    {
+        private Dictionary<T, GraphNode<T>> nodeDict = new Dictionary<T, GraphNode<T>>();
+
+        /// <summary>
+        /// The dictionary of nodes.
+        /// </summary>
+        public IReadOnlyDictionary<T, GraphNode<T>> NodeDict => nodeDict;
+
+        /// <summary>
+        /// The collection of nodes.
+        /// </summary>
+        public IEnumerable<GraphNode<T>> Nodes => nodeDict.Values;
+
+        /// <summary>
+        /// Retrieves the node with value T.
+        /// </summary>
+        public GraphNode<T> this [T value] => nodeDict[value];
+
+        /// <summary>
+        /// Create nodes for the given values and add them to this graph.
+        /// </summary>
+        /// <param name="values">The values to add.</param>
+        public void AddRange(IEnumerable<T> values)
+        {
+            foreach (var value in values)
+                nodeDict.Add(value, new GraphNode<T>(value));
+        }
+
+        /// <summary>
+        /// Looks up nodes/creates nodes based on values and attaches nodes tp parent.
+        /// </summary>
+        /// <param name="parent">Parent node value.</param>
+        /// <param name="children">Values of children to add.</param>
+        public void SetChildren(T parent, params T[] children)
+        {
+            if (!nodeDict.TryGetValue(parent, out var parentNode))
+            {
+                parentNode = new GraphNode<T>(parent);
+                nodeDict.Add(parent, parentNode);
+            }
+
+            foreach (var child in children)
+            {
+                if (!nodeDict.TryGetValue(child, out var childNode))
+                {
+                    childNode = new GraphNode<T>(child);
+                    nodeDict.Add(child, childNode);
+                }
+                parentNode.AddChild(childNode);
+            }
+        }
+    }
+}
--- Tests/Chapter04Tests.cs
+++ Tests/Chapter04Tests.cs
@@ -11,33 +11,6 @@ namespace Tests
     {
         private IntNode testTree;
 
-        [Test]
-        public void GraphTest()
-        {
-            var graph = new SetGraph<int>();
-            graph.SetChildren(4, 6);
-            graph.SetChildren(5, 4);
-            graph.SetChildren(6, 5);
-
-            graph.SetChildren(2, 0, 3);
-            graph.SetChildren(3, 2);
-            graph.SetChildren(1, 2);
-            graph.SetChildren(0, 1);
-
-            foreach (var node in graph.Nodes)
-                Console.WriteLine(node);
-
-            var node0 = graph[0];
-            var node2 = graph[2];
-            var node3 = graph[3];
-
-            Assert.That(node2.Children, Has.Member(node3));
-            Assert.That(node2.Children, Has.Member(node0));
-
-            Assert.That(node3.Children, Has.Member(node2));
-            Assert.That(node0.Children, Has.No.Member(node2));
-        }
-
         /// <summary>
         /// Create the test fixture example tree.
         /// </summary>
