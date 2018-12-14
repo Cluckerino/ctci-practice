487e79e9d817795b9cb3f08b20f9dbef06052e19
Cluckerino <elitedragonslayer@gmail.com>
2018-12-13 19:06:01 -0800

    Solution for 16.6

--- Problems/Chapter16.cs
+++ Problems/Chapter16.cs
@@ -1,4 +1,6 @@
-﻿namespace Problems
+﻿using System;
+
+namespace Problems
 {
     public static class Chapter16
     {
@@ -19,7 +21,29 @@
         /// </summary>
         public static int P06SmallestDifference(int[] a, int[] b)
         {
-            return 0;
+            Array.Sort(a);
+            Array.Sort(b);
+
+            var aInd = a.Length - 1;
+            var bInd = b.Length - 1;
+            // Initialize to something high.
+            var minDiff = int.MaxValue;
+
+            while (aInd >= 0 && bInd >= 0)
+            {
+                var aVal = a[aInd];
+                var bVal = b[bInd];
+                var curDiff = Math.Abs(bVal - aVal);
+                if (curDiff < minDiff)
+                {
+                    minDiff = curDiff;
+                }
+
+                if (aVal > bVal) aInd--;
+                else bInd--;
+            }
+
+            return minDiff;
         }
     }
 }
