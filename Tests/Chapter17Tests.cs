cfe90303a2323db26950ee6fea6ed782af9a7989
Cluckerino <elitedragonslayer@gmail.com>
2018-12-13 21:51:15 -0800

    Solution for 17.26

--- Tests/Chapter17Tests.cs
+++ Tests/Chapter17Tests.cs
@@ -7,6 +7,11 @@ namespace Tests
     [TestFixture]
     public class Chapter17Tests
     {
+        /// <summary>
+        /// Tolerance for comparing doubles.
+        /// </summary>
+        private const double Tolerance = 1E-12;
+
         private Dictionary<int, int[]> sparseSimDocs;
 
         [SetUp]
@@ -23,7 +28,33 @@ namespace Tests
             var docA = sparseSimDocs[docAId];
             var docB = sparseSimDocs[docBId];
             var actual = Chapter17.P26SparseSimilarity(docA, docB);
-            Assert.That(actual, Is.EqualTo(expected).Within(1E-12));
+            Assert.That(actual, Is.EqualTo(expected).Within(Tolerance));
+        }
+
+        [Test]
+        public void T26SparseSimilarityAllDocs()
+        {
+            var expected = new Dictionary<string, double>
+                {
+                    // Random comment to get linter to behave.
+                    { Chapter17.IdPair(13, 19), 0.1 },
+                    { Chapter17.IdPair(13, 16), 0.25 },
+                    { Chapter17.IdPair(19, 24), 0.14285714285714286 }
+                };
+
+            var actual = Chapter17.P26SparseSimilarityAllDocs(sparseSimDocs);
+
+            Assert.That(actual.Count, Is.EqualTo(expected.Count));
+            Assert.That(actual.Keys, Is.EquivalentTo(expected.Keys));
+
+            // Compare similarities
+            foreach (var expKV in expected)
+            {
+                var expectedSim = expKV.Value;
+                var actualSim = actual[expKV.Key];
+
+                Assert.That(actualSim, Is.EqualTo(expectedSim).Within(Tolerance));
+            }
         }
 
         /// <summary>
